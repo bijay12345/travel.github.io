b1a96a52dde886b0233c89c56a937ba2c93f566b
bijay12345 <sbijay433@gmail.com>
2023-02-09 10:47:58 +0530

    Updates

---
+++ precache-manifest.06c57fd0bd2608b9f641ce4312b38de6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "09cc58f02f14ba6d8377",
+    "url": "/bijay12345/travel.github.io.git/static/css/main.122d435a.chunk.css"
+  },
+  {
+    "revision": "09cc58f02f14ba6d8377",
+    "url": "/bijay12345/travel.github.io.git/static/js/main.09cc58f0.chunk.js"
+  },
+  {
+    "revision": "467d666577b566840c31",
+    "url": "/bijay12345/travel.github.io.git/static/js/1.467d6665.chunk.js"
+  },
+  {
+    "revision": "082cfa066461e30b1cee",
+    "url": "/bijay12345/travel.github.io.git/static/js/runtime~main.082cfa06.js"
+  },
+  {
+    "revision": "cae72840781a4bf46b049dc83ec6ae4f",
+    "url": "/bijay12345/travel.github.io.git/static/media/1.cae72840.jpg"
+  },
+  {
+    "revision": "7cb3d913ca6197cfa9e6d7d197588335",
+    "url": "/bijay12345/travel.github.io.git/static/media/2.7cb3d913.jpg"
+  },
+  {
+    "revision": "da0af984a6eaf438e8cc157c71e96268",
+    "url": "/bijay12345/travel.github.io.git/static/media/3.da0af984.jpg"
+  },
+  {
+    "revision": "3706dacadcef406a6483c69cc4aff5f8",
+    "url": "/bijay12345/travel.github.io.git/static/media/4.3706daca.jpg"
+  },
+  {
+    "revision": "32ac13a63f0ab9d987d2ba9c4b9349c1",
+    "url": "/bijay12345/travel.github.io.git/static/media/11.32ac13a6.jpg"
+  },
+  {
+    "revision": "cf7f2b0640dfe908b8cde51c72a65cf2",
+    "url": "/bijay12345/travel.github.io.git/static/media/8.cf7f2b06.jpg"
+  },
+  {
+    "revision": "74fce4d5d8cad3c0ba03fc6e3f30e7b2",
+    "url": "/bijay12345/travel.github.io.git/static/media/6.74fce4d5.jpg"
+  },
+  {
+    "revision": "cc3ac5f8f5deccd2cedc87182220947e",
+    "url": "/bijay12345/travel.github.io.git/static/media/night.cc3ac5f8.jpg"
+  },
+  {
+    "revision": "97f00fa7b8798987ffde7be9501b208a",
+    "url": "/bijay12345/travel.github.io.git/static/media/9.97f00fa7.jpg"
+  },
+  {
+    "revision": "e1d2d75caad0ca5a8e971c466777c5a6",
+    "url": "/bijay12345/travel.github.io.git/index.html"
+  }
+];
